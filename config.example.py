9ab45767a5b96c67b5c6a8973104a98481aff71a
Tiago Teles <tiago.sequeira.teles@gmail.com>
2020-10-12 03:12:32 +0100

    Added script to tell you which class comes next

--- config.example.py
+++ config.example.py
@@ -1,2 +1,4 @@
+from pytz import timezone
 # private calendar URL, google ones look like this
 CALENDAR_URL = "https://calendar.google.com/calendar/ical/foo/private-bar/basic.ics"
+TZ = timezone("Europe/Lisbon")
---
+++ countdown.py
@@ -0,0 +1,54 @@
+import icalendar
+import recurring_ical_events as recurring
+import os
+import sys
+from datetime import date, datetime, timedelta
+
+from config import CALENDAR_URL, TZ
+
+# Change to current file's folder
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+# Just an alias
+now = datetime.now(TZ)
+
+
+def pretty_print_time_until(time):
+    delta = time - now
+    hours = int(delta.total_seconds() / 3600)
+    minutes = int((delta.total_seconds() % 3600) / 60)
+    if delta < timedelta(hours=1):
+        return str(minutes) + "m"
+    elif delta < timedelta(hours=3):
+        return str(hours) + "h" + str(minutes) + "m"
+    else:
+        return str(hours) + " hours"
+
+
+# Grab calendar from file
+cal_text = open("./calendar.ics").read()
+cal = icalendar.Calendar.from_ical(cal_text)
+
+today_events = recurring.of(cal).at(date.today())
+events = []
+for event in today_events:
+    if event.decoded("dtend") > now:
+        events.append(event)
+
+events.sort(key=lambda x: x.decoded("dtstart"))
+
+# Types of prints
+if len(events) == 0:
+    sys.exit(1)
+if events[0].decoded("dtstart") > now:
+    print(events[0].decoded("summary").decode() + " in " +
+          pretty_print_time_until(events[0].decoded("dtstart")))
+elif len(events) >= 2:
+    print(events[0].decoded("summary").decode() + " ends in " +
+          pretty_print_time_until(events[0].decoded("dtend")) +
+          ". " + events[1].decoded("summary").decode() + " after a " + pretty_print_time_until(events[1].decoded("dtstart") - events[0].decoded("dtend") + now) + " break.")
+else:
+    print(events[0].decoded("summary").decode() + " ends in " +
+          pretty_print_time_until(events[0].decoded("dtend")))
